191b5db9c4525bfab5ef53b3037610d0916ce1cd
M12Zidan <maulanazidan12@gmail.com>
2025-10-09 08:18:57 +0700

    fix:fixing for deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,35 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
-  ...compat.extends(["eslint:recommended", "plugin:react/recommended"]),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ),
   {
-    ignores: ["node_modules/**", "dist/**", "build/**", ".next/**"],
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "dist/**",
+      "next-env.d.ts",
+    ],
   },
 ];
 
 export default eslintConfig;
+
 
 // import { dirname } from "path"; import { fileURLToPath } from "url"; import { FlatCompat } from "@eslint/eslintrc"; const __filename = fileURLToPath(import.meta.url); const __dirname = dirname(__filename); const compat = new FlatCompat({ baseDirectory: __dirname, }); const eslintConfig = [ ...compat.extends("next/core-web-vitals"), { ignores: [ "node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts", ], }, ]; export default eslintConfig;
