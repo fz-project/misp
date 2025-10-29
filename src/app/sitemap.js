4ddf4f25f789e56ac29ab583c931c3d4e2d6de8b
M12Zidan <maulanazidan12@gmail.com>
2025-10-29 10:54:18 +0700

    feat: update sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://misp.co.id',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}
